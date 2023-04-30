9558bb3946c11d7bf6f5d5f71c379e129797cd05
growing mistake <113902138+100tari@users.noreply.github.com>
2023-04-30 04:09:43 -0700

    Update header1.h
    adding generateKey

--- header1.h
+++ header1.h
@@ -3,7 +3,12 @@
 #include <stdint.h>
 
 #define NUMBER_SIZE 200000
-#define  BUFFER_SIZE NUMBER_SIZE / 301
+#define BUFFER_SIZE NUMBER_SIZE / 301
 
 typedef uint64_t key_part;
 typedef key_part buffer[BUFFER_SIZE];
+
+void generatePrivateKey(buffer b) {
+    for(int i = 0; i < BUFFER_SIZE; i++)
+        b[i] = rand();
+}
