f35a6fa687cdad97e34809119ee16c930ff337b9
AliBhd1024 <117184081+AliBhd1024@users.noreply.github.com>
2023-05-01 18:56:01 +0330

    Client Sketch

---
+++ sketch1.ino.ino
@@ -0,0 +1,50 @@
+#include <WiFi.h>
+
+const char* ssid = "me";
+const char* password = "sadtari38";
+
+WiFiServer server(80);
+
+void setup() {
+    Serial.begin(115200);
+
+    // Connect to Wi-Fi network
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+
+    Serial.println("Connected to WiFi");
+
+    // Start server
+    
+    server.begin();
+}
+
+void loop() {
+    // Check if a client has connected
+    WiFiClient client = server.available();
+    if (client) {
+        Serial.println("New client connected");
+
+        // Wait for data from client
+        while (client.connected()) {
+            if (client.available()) {
+                String request = client.readStringUntil('\r');
+                Serial.println(request);
+
+                // Send response to client
+                client.println("HTTP/1.1 200 OK");
+                client.println("Content-Type: text/html");
+                client.println("");
+                client.println("<html><body><h1>Hello, World!</h1></body></html>");
+                break;
+            }
+        }
+
+        // Close the connection
+        client.stop();
+        Serial.println("Client disconnected");
+    }
+}
