35a4047330d5e25a9477bcdff361f518f8fd7df1
growing mistake <113902138+100tari@users.noreply.github.com>
2023-05-01 13:46:32 -0700

    Update sketch_2.ino
    changing server variable to WiFiServer

--- sketch_2.ino
+++ sketch_2.ino
@@ -1,24 +1,19 @@
-#include "WiFi.h"
-#include "ESPAsyncWebServer-master/src/ESPAsyncWebServer.h"
-#include <Wire.h>
+#include <WiFi.h>
 
-// Set your access point network credentials
-const char* ssid = "ESP32-Access-Point";
+// Replace with your network credentials
+const char* ssid     = "ESP32-Access-Point";
 const char* password = "123456789";
 
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
+// Set web server port number to 80
+WiFiServer server(80);
 
-String readTemp() {
-  return "Hello World!";
-}
+// Variable to store the HTTP request
+String header;
 
-void setup(){
-  // Serial port for debugging purposes
+void setup() {
   Serial.begin(115200);
-  Serial.println();
-  
-  // Setting the ESP as an access point
+
+  // Connect to Wi-Fi network with SSID and password
   Serial.print("Setting AP (Access Point)…");
   // Remove the password parameter, if you want the AP (Access Point) to be open
   WiFi.softAP(ssid, password);
@@ -26,15 +21,52 @@ void setup(){
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(IP);
-
-  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", readTemp().c_str());
-  });
   
-  // Start server
   server.begin();
 }
- 
+
 void loop(){
-  
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    Serial.println(client.localIP());
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            client.println("message");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
