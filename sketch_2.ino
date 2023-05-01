a2a07f52e7e1d3973792b03a1a4e0e8becba34eb
AliBhd1024 <117184081+AliBhd1024@users.noreply.github.com>
2023-05-01 18:18:28 +0330

    Access Point Sketch

---
+++ sketch_2.ino
@@ -0,0 +1,40 @@
+#include "WiFi.h"
+#include "ESPAsyncWebServer-master/src/ESPAsyncWebServer.h"
+#include <Wire.h>
+
+// Set your access point network credentials
+const char* ssid = "ESP32-Access-Point";
+const char* password = "123456789";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+String readTemp() {
+  return "Hello World!";
+}
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  Serial.println();
+  
+  // Setting the ESP as an access point
+  Serial.print("Setting AP (Access Point)…");
+  // Remove the password parameter, if you want the AP (Access Point) to be open
+  WiFi.softAP(ssid, password);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readTemp().c_str());
+  });
+  
+  // Start server
+  server.begin();
+}
+ 
+void loop(){
+  
+}
