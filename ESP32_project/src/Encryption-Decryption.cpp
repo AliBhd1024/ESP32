fb851c1c9bb502caec7f6718e464d96ab26aa5b2
AliBhd1024 <117184081+AliBhd1024@users.noreply.github.com>
2023-05-09 18:37:03 +0330

    Update Encryption-Decryption.cpp

--- ESP32_project/src/Encryption-Decryption.cpp
+++ ESP32_project/src/Encryption-Decryption.cpp
@@ -1,33 +1,41 @@
+#include <mbedtls/aes.h>
 #include <Arduino.h>
-#include <AES.h>
+#include <mbedtls/platform.h>
 
-AES aes(AESKeyLength::AES_128);
-unsigned char KEY[16];
+u_char ciphertext[16] = {};
+u_char KEY1[16] = {};
+u_char plaintext[16] = {} ;
+u_char plain[16] = "FUCKYOU AESFuCK";
+
+mbedtls_aes_context aes;
 
 void setKEY(uint64_t key) {
+    mbedtls_aes_init(&aes);
+
     String keystr(key);
     int c = 0 , len = keystr.length();
 
-    while(c++ != 16) {
-        KEY[c] = keystr[c % len];
+    while(c != 16) {
+        KEY1[c] = keystr[c % len];
+
+        c++;
     }
+
+    mbedtls_aes_setkey_enc(&aes , KEY1 , 128);
 }
 
 
 String encMsg(String text) {
-    unsigned char parts[text.length()];
+    mbedtls_aes_crypt_ecb(&aes , MBEDTLS_AES_ENCRYPT , plain , ciphertext);
+    String ctext(ciphertext , 16);
+    return ctext;
 
-    text.toCharArray((char *)parts , text.length());
-
-    String cipher (aes.EncryptECB(parts , text.length() , KEY) , text.length());
-    return cipher;
+    // mbedtls_aes_free(&aes);
 }
 
+
 String decMsg(String text) {
-    unsigned char parts[text.length()];
-
-    text.toCharArray((char *)parts , text.length());
-
-    String cipher (aes.DecryptECB(parts , text.length() , KEY) , text.length());
-    return cipher;
+    mbedtls_aes_decrypt(&aes , ciphertext , plaintext);
+    String ptext(plaintext , 16);
+    return ptext;
 }
